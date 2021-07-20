83a4dee0881582072e6604e4ffb4e352a58d0a52
philly <ph.stewart@icloud.com>
2021-07-20 19:06:10 -0400

    fr

--- public/budgetDB.js
+++ public/budgetDB.js
@@ -1,3 +1,5 @@
+const { get } = require("mongoose");
+
 let dataB
 let newBudget
 
@@ -16,6 +18,44 @@ if (dataB.objectStoreNames.lenth === 0) {
     dataB.createObjectStore('BudgetStore', {autoIncrement: true })
 }};
 
+request.onerror = function (e) {
+    console.log(`Woops! ${e.target.errorCode}`);
+  };
 
+function workingDB() {
 
+let transaction = dataB.transaction(['BudgetStore'], 'readwrite');
 
+const save = transaction.objectStore('BudgetStore');
+
+const getAll = save.getAll()
+
+getAll.onsucess = function() {
+
+    if (getAll.result.length > 0) {
+        fetch('/api/transaction/bulk', {
+          method: 'POST',
+          body: JSON.stringify(getAll.result),
+          headers: {
+            Accept: 'application/json, text/plain, */*',
+            'Content-Type': 'application/json',
+          },
+        })
+          .then((response) => response.json())
+          .then((res) => {
+            // If our returned response is not empty
+            if (res.length !== 0) {
+              // Open another transaction to BudgetStore with the ability to read and write
+              transaction = db.transaction(['BudgetStore'], 'readwrite');
+  
+              // Assign the current store to a variable
+              const currentStore = transaction.objectStore('BudgetStore');
+              currentStore.clear();
+              console.log("clearing store");
+            }
+          });
+      }
+
+}
+
+}
