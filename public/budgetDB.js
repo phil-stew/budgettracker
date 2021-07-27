19a2ee2861888ef6c5cbd395b5d477d952e008d4
philly <ph.stewart@icloud.com>
2021-07-26 20:11:51 -0400

    offline works now

--- public/budgetDB.js
+++ public/budgetDB.js
@@ -1,94 +1,74 @@
-//const { get } = require("mongoose");
+const indexedDB =
+  window.indexedDB ||
+  window.mozIndexedDB ||
+  window.webkitIndexedDB ||
+  window.msIndexedDB ||
+  window.shimIndexedDB;
 
-let dataB
+let dataB;
 let newBudget
 
-const request = window.indexedDB.open('BudgetData', newBudget || 21)
+const request = indexedDB.open('BudgetData', 1)
 
-request.onupgradeneeded = function (e) {
-    console.log('Upgraded Needed')
+request.onupgradeneeded = (e) => {
+  console.log('Upgraded Needed')
 
-    const { oldVersion } = e;
-    const newVersion = e.newVersion || dataB.version
-console.log(`Updated from version ${oldVersion} to ${newVersion}`);
+  const { oldVersion } = e;
+  const newVersion = e.newVersion || dataB.version
+  console.log(`Updated from version ${oldVersion} to ${newVersion}`);
 
-dataB = e.target.result;
-
-if (dataB.objectStoreNames.lenth === 0) {
-    dataB.createObjectStore('BudgetStore', {autoIncrement: true })
-}};
+  let dataB = e.target.result;
+  dataB.createObjectStore('BudgetStore', { autoIncrement: true })
+};
 
 request.onerror = function (e) {
-    console.log(`Woops! ${e.target.errorCode}`);
-  };
+  console.log(`Woops! ${e.target.errorCode}`);
+};
 
 request.onsuccess = function (e) {
   console.log('success')
   dataB = e.target.result;
+  if (navigator.online) {
+    console.log('backend online')
+    workingDB()
+  }
 }
-
-
-if (navigator.online){
-  console.log('backend online')
-  workingDB()
-}
-
-
-
-
-
-
-
 
 function workingDB() {
 
-let transaction = dataB.transaction(['BudgetStore'], 'readwrite');
+  let transaction = dataB.transaction(['BudgetStore'], 'readwrite');
 
-const store = transaction.objectStore('BudgetStore');
+  const store = transaction.objectStore('BudgetStore');
 
-const getAll = store.getAll()
+  const getAll = store.getAll()
 
-getAll.onsucess = function() {
+  getAll.onsuccess = function () {
 
     if (getAll.result.length > 0) {
-        fetch('/api/transaction/bulk', {
-          method: 'POST',
-          body: JSON.stringify(getAll.result),
-          headers: {
-            Accept: 'application/json, text/plain, */*',
-            'Content-Type': 'application/json',
-          },
-        })
-          .then((response) => response.json())
-          .then((res) => {
-          
-            if (res.length !== 0) {
-              
-              transaction = dataB.transaction(['BudgetStore'], 'readwrite');
-  
- 
-              const currentstore = transaction.objectStore('BudgetStore');
-              currentstore.clear();
-              console.log("clearing store");
-            }
-          });
-      }}}
-
-request.onsuccess = function (e) {
-    console.log('success');
-    dataB = e.target.result;
-
-    if (navigator.onLine) {
-        console.log('Backend online!');
-        workingDB();
-      }
-    };
+      fetch('/api/transaction', {
+        method: 'POST',
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json',
+        },
+      })
+        .then((response) => response.json())
+        .then(() => {
+          const transaction = dataB.transaction(['BudgetStore'], 'readwrite');
+          const currentstore = transaction.objectStore('BudgetStore');
+          currentstore.clear();
+          console.log("clearing store");
+        });
+    }
+  }
+}
 
 const saveRecord = (record) => {
-    console.log('store record invoked');
-    const transaction = dataB.transaction(['BudgetStore'], 'readwrite');
-    const store = transaction.objectStore('BudgetStore');
-    storeRecord.add(record);
-  };
+  console.log('store record invoked');
+  const transaction = dataB.transaction(['BudgetStore'], 'readwrite');
+  const store = transaction.objectStore('BudgetStore');
+  store.add(record);
+};
 
-  window.addEventListener('online', workingDB);
+window.addEventListener('online', workingDB);
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,69 +1,67 @@
+const filesToCache = [
+  "/",
+  "/budgetDB.js",
+  "/index.js",
+  "/manifest.webmanifest",
+  "/styles.css",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
+];
 
-// self.addEventListener("fetch", event => {
-//   console.log("You fetched " + event.url);
-// });
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
 
 
 
-const FILES_TO_CACHE = [
-    "/",
-    "/index.html",
-    "/index.js",
-    "/manifest.webmanifest",
-    "/styles.css",
-    "/icons/icon-192x192.png",
-    "/icons/icon-512x512.png",
-    "/BudgetDB.js", 
-    'https://fonts.googleapis.com/css?family=Istok+Web|Montserrat:800&display=swap',
-    'https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css',
-  ];
-  
-  const CACHE_NAME = "static-cache-v2";
-  const DATA_CACHE_NAME = "data-cache-v1";
-  
+self.addEventListener('install', function (event) {
 
-  
-  self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then(function (cache) {
+        console.log("Cache deez files")
+        return cache.addAll(filesToCache)
+      })
+  );
+});
 
-    event.waitUntil(
-      caches
-        .open(CACHE_NAME)
-        .then((cache) => cache.addAll(FILES_TO_CACHE))
-        .then(self.skipWaiting())
-    );
-  });
-  
-  // The activate handler takes care of cleaning up old caches.
-  self.addEventListener('activate', (event) => {
+// The activate handler takes care of cleaning up old caches.
+self.addEventListener('activate', (event) => {
   const currentCaches = [CACHE_NAME, DATA_CACHE_NAME];
-    event.waitUntil(
-      caches
-        .keys()
-        .then((cacheNames) => {
-          return cacheNames.filter((cacheName) => !currentCaches.includes(cacheName));
-        })
-        .then((cachesToDelete) => {
-          return Promise.all(
-            cachesToDelete.map((cacheToDelete) => {
-              return caches.delete(cacheToDelete);
-            })
-          );
-        })
-        .then(() => self.clients.claim())
-    );
-  });
-  
+  event.waitUntil(
+    caches
+      .keys()
+      .then((cacheNames) => {
+        return cacheNames.filter((cacheName) => !currentCaches.includes(cacheName));
+      })
+      .then((cachesToDelete) => {
+        return Promise.all(
+          cachesToDelete.map((cacheToDelete) => {
+            return caches.delete(cacheToDelete);
+          })
+        );
+      })
+      .then(() => self.clients.claim())
+  );
+});
 
-  
-  self.addEventListener('fetch', (event) => {
-    if (event.request.url.includes('/api/')) {
-      event.respondWith(
-         caches.open(DATA_CACHE_NAME).then(async (cache) => {
-            const response = await fetch(event.request);
-           await cache.put(event.request, response.clone());
-           return response;
-        })
-      );
-    }
-  });
-  
+
+
+self.addEventListener('fetch', function(event) {
+  if (event.request.url.includes('/api/')) {
+    event.respondWith(
+      caches.open(DATA_CACHE_NAME).then((cache) => {
+        return fetch(event.request)
+          .then(response => {
+            if (response.status === 200) {
+              cache.put(event.request.url, response.clone());
+            }
+            return response;
+          })
+          .catch(err => {
+            return cache.match(event.request);
+          });
+      }).catch(err => console.log(err))
+    );
+  }
+});
